0dbb22587e21c18fbfb520bd11a1b379b1d257fc
richod@1 <frimpsup@gmail.com>
2023-03-01 17:30:28 -0800

    client connit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Home from './components/Home';
+import Resume from './components/Resume'
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import React, {useState} from 'react'
 
 function App() {
+  const [result,setResult]=useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home setResult={setResult}/>}/>
+        <Route path='/resume' element={<Resume result={result}/>}/>
+      </Routes>
+
     </div>
   );
 }
